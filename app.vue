00083e13ebd130dd25d48a4f8345a3bc5c9f61b5
Arnold <antaloczi.arnold@gmail.com>
2024-06-25 16:14:48 +0200

    Set base app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="greeting">
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const title = ref<string>('Üdvözöljük a Vue.js-ban!')
+const message = ref<string>('Ez egy egyszerű komponens példája.')
+</script>
+
+<style scoped>
+
+.greeting {
+  text-align: center;
+  margin-top: 50px;
+}
+
+h1 {
+  color: #42b983;
+}
+
+p {
+  color: #35495e;
+}
+</style>
