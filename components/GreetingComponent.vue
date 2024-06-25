c99eb8af78395930e3781069cba9f836d14353e3
Arnold <antaloczi.arnold@gmail.com>
2024-06-25 20:53:48 +0200

    Separate component

---
+++ components/GreetingComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="greeting">
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+  
+<script lang="ts" setup>
+  const title = ref<string>("Üdvözöljük a Vue.js-ban!");
+  const message = ref<string>("Ez egy egyszerű komponens példája.");
+  
+  // Angularban az ngOnInit
+  onMounted(() => {
+    console.log("onMounted lefutott");
+  
+    setTimeout(() => {
+      title.value = "Lecserélt title!";
+    }, 5000);
+  });
+  
+  // Angularban az ngOnDestroy
+  onBeforeUnmount(() => {
+    console.log("onBeforeUnmount lefutott");
+  });
+</script>
+  
+<style lang="scss" scoped>
+  .greeting {
+    text-align: center;
+    margin-top: 50px;
+  }
+  
+  h1 {
+    color: #42b983;
+  }
+  
+  p {
+    color: #35495e;
+  }
+</style>
--- plugins/bootstrap.js
+++ plugins/bootstrap.js
@@ -1,4 +1,3 @@
-// plugins/bootstrap.js
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 export default defineNuxtPlugin(() => { });
