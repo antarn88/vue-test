1ede5cf656b01a8590757b898bdb99dfabbde144
Arnold <antaloczi.arnold@gmail.com>
2024-06-26 13:48:41 +0200

    Run autofix linter

--- components/GreetingComponent.vue
+++ components/GreetingComponent.vue
@@ -4,37 +4,39 @@
     <p>{{ message }}</p>
   </div>
 </template>
-  
+
 <script lang="ts" setup>
-  const title = ref<string>("Üdvözöljük a Vue.js-ban!");
-  const message = ref<string>("Ez egy egyszerű komponens példája.");
-  
-  // Angularban az ngOnInit
-  onMounted(() => {
-    console.log("onMounted lefutott");
-  
-    setTimeout(() => {
-      title.value = "Lecserélt title!";
-    }, 5000);
-  });
-  
-  // Angularban az ngOnDestroy
-  onBeforeUnmount(() => {
-    console.log("onBeforeUnmount lefutott");
-  });
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+
+const title = ref<string>('Üdvözöljük a Vue.js-ban!');
+const message = ref<string>('Ez egy egyszerű komponens példája.');
+
+// Angularban az ngOnInit
+onMounted(() => {
+  console.log('onMounted lefutott');
+
+  setTimeout(() => {
+    title.value = 'Lecserélt title!';
+  }, 5000);
+});
+
+// Angularban az ngOnDestroy
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount lefutott');
+});
 </script>
-  
+
 <style lang="scss" scoped>
-  .greeting {
-    text-align: center;
-    margin-top: 50px;
-  }
-  
-  h1 {
-    color: #42b983;
-  }
-  
-  p {
-    color: #35495e;
-  }
+.greeting {
+  text-align: center;
+  margin-top: 50px;
+}
+
+h1 {
+  color: #42b983;
+}
+
+p {
+  color: #35495e;
+}
 </style>
