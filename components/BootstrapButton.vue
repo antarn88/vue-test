30e527cad806e6ad3bc9b6a355cc07e1494d723a
Arnold <antaloczi.arnold@gmail.com>
2024-06-26 19:16:36 +0200

    Input-output component example

---
+++ components/BootstrapButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- ':class' = dinamikus class hozzárendelés, akár feltételesen is -->
+  <button class="btn" :class="['btn-' + color, 'btn-' + size]" @click="emitClick">{{ label }}</button>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, defineEmits } from 'vue';
+
+const emit = defineEmits(['click']); // defineEmits(['click', 'hover', 'focus']);
+
+// Input propok, amiket definiálunk
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: 'primary',
+  },
+  size: {
+    type: String,
+    default: 'md',
+  },
+});
+
+function emitClick(): void {
+  const customData = {
+    message: 'Output adat szövege',
+    timestamp: new Date().toISOString(),
+  };
+
+  emit('click', customData);
+}
+</script>
+
+<style scoped>
+/* Itt lehet egyedi stílusokat hozzáadni, ha szükséges */
+</style>
--- components/GreetingComponent.vue
+++ components/GreetingComponent.vue
@@ -3,10 +3,21 @@
     <h1>{{ title }}</h1>
     <p>{{ message }}</p>
   </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <!-- Input adatként megadjuk a labelt, colort és size-ot -->
+        <!-- A click eseményre pedig a handleClick fog lefutni a bejövő adatokkal. -->
+        <BootstrapButton label="Klikk ide!" color="primary" size="md" @click="handleClick" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { onBeforeUnmount, onMounted, ref } from 'vue';
+import type { ClickData } from '~/models/ClickData'; // Típus importálás
 
 const title = ref<string>('Üdvözöljük a Vue.js-ban!');
 const message = ref<string>('Ez egy egyszerű komponens példája.');
@@ -24,6 +35,11 @@ onMounted(() => {
 onBeforeUnmount(() => {
   console.log('onBeforeUnmount lefutott');
 });
+
+// Eseménykezelő
+function handleClick(outputData: ClickData): void {
+  console.log('A gombra klikkeltél, a bejövő adat:', outputData);
+}
 </script>
 
 <style lang="scss" scoped>
