f920c6b4689bf66bafb34f28aef817ce1fabde3e
Arnold <antaloczi.arnold@gmail.com>
2024-06-27 13:23:05 +0200

    Test v-for, v-if, v-show

--- src/components/GreetingComponent.vue
+++ src/components/GreetingComponent.vue
@@ -1,58 +1,39 @@
 <template>
-  <div class="greeting">
-    <h1>{{ title }}</h1>
-    <p>{{ message }}</p>
-  </div>
-
   <div class="container">
-    <div class="row">
+    <div class="row mt-5">
       <div class="col-6">
-        <!-- Input adatként megadjuk a labelt, colort és size-ot -->
-        <!-- A click eseményre pedig a handleClick fog lefutni a bejövő adatokkal. -->
-        <BootstrapButton label="Klikk ide!" color="primary" size="md" @click="handleClick" />
+        <button class="btn btn-primary" @click="toggleVisibility">Toggle Visibility</button>
+
+        <!-- v-if -> Ha nem látszik, akkor a DOM-ból is eltűnik az elem! Angularban ez ngIf -->
+        <p v-if="isVisible">Ez egy feltételesen megjelenített szöveg.</p>
+
+        <!-- v-show -> Ha nem látszik, a DOM-ban ettől még megjelenik! Angularban ez hidden -->
+        <p v-show="isVisible">Ez egy feltételesen elrejtett szöveg.</p>
+      </div>
+
+      <div class="col-6">
+        <ul>
+          <h5>Nevek kiírása ciklussal</h5>
+          <!--v-for -> Angularban ez az ngFor -->
+          <p v-for="name in names" :key="name.id" class="mb-0">{{ name.name }}</p>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onBeforeUnmount, onMounted, ref } from 'vue';
-import type { ClickData } from '~/models/ClickData';
+import { ref } from 'vue';
 
-const title = ref<string>('Üdvözöljük a Vue.js-ban!');
-const message = ref<string>('Ez egy egyszerű komponens példája.');
+const isVisible = ref(true);
+const names = ref([
+  { id: '1', name: 'Gipsz Jakab' },
+  { id: '2', name: 'Nagy Béla' },
+  { id: '3', name: 'Kiss Zsolt' },
+  { id: '4', name: 'Szabó Ernő' },
+]);
 
-// Angularban az ngOnInit
-onMounted(() => {
-  console.log('onMounted lefutott');
-
-  setTimeout(() => {
-    title.value = 'Lecserélt title!';
-  }, 5000);
-});
-
-// Angularban az ngOnDestroy
-onBeforeUnmount(() => {
-  console.log('onBeforeUnmount lefutott');
-});
-
-// Eseménykezelő
-function handleClick(outputData: ClickData): void {
-  console.log('A gombra klikkeltél, a bejövő adat:', outputData);
+function toggleVisibility(): void {
+  isVisible.value = !isVisible.value;
 }
 </script>
-
-<style lang="scss" scoped>
-.greeting {
-  text-align: center;
-  margin-top: 50px;
-}
-
-h1 {
-  color: #42b983;
-}
-
-p {
-  color: #35495e;
-}
-</style>
