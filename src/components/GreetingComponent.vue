daa9740a19c2a110ec33fc81dee054ce8eb2ee20
Arnold <antaloczi.arnold@gmail.com>
2024-06-28 18:01:30 +0200

    Update eslint, prettier and settings, and use computed (like pipes)

--- src/components/GreetingComponent.vue
+++ src/components/GreetingComponent.vue
@@ -21,11 +21,21 @@
         </ul>
       </div>
     </div>
+
+    <!-- Összegek szekció -->
+    <div class="row mt-5">
+      <div class="col">
+        <ul>
+          <h5>Összegek (pipe szerű cuccal megoldva)</h5>
+          <p v-for="(amount, index) in formattedAmounts" :key="index" class="mb-0">{{ amount }}</p>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue';
+  import { ref, computed } from 'vue';
 
   const isVisible = ref(true);
   const names = ref([
@@ -34,6 +44,23 @@
     { id: '3', name: 'Kiss Zsolt' },
     { id: '4', name: 'Szabó Ernő' },
   ]);
+
+  const amounts = ref([145, 787, 1215, 1219, 78787]);
+  const formattedAmounts = computed(() => amounts.value.map((amount: number) => formatMoney(amount)));
+
+  const formatMoney = (amount: number) => {
+    if (amount >= 1000 && amount < 10000) {
+      return `${amount.toString()[0]} ${amount.toString().slice(-3)} Ft`;
+    }
+
+    const formatter = new Intl.NumberFormat('hu-HU', {
+      style: 'currency',
+      currency: 'HUF',
+      maximumFractionDigits: 0,
+    });
+
+    return formatter.format(amount);
+  };
 
   function toggleVisibility(): void {
     isVisible.value = !isVisible.value;
